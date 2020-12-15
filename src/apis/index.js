4ce2259c61a9a07be38f6246d13de01c40e17832
hiep <hiep@gmail.com>
2020-12-16 06:58:35 +0700

    modify app.js

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,10 +2,11 @@ const productRoutes = require('./routers/product');
 const categoryRoutes = require('./routers/category');
 const userRoutes = require('./routers/user');
 const orderRoutes = require('./routers/order');
+const cors = require('cors');
 
 module.exports = app => {
     app.use('/products', productRoutes);
     app.use('/categories', categoryRoutes);
     app.use('/users', userRoutes);
-    app.use('/orders', orderRoutes);
+    app.use('/orders', cors(), orderRoutes);
 };
--- src/app.js
+++ src/app.js
@@ -1,27 +1,17 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerDocument = require("./swagger.json");
-// require("./connections/mongodb-local");
 require("./connections/mongodb-atlas");
 const configRoute = require("./apis/index");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(cors());
-app.get("/*", (req, res, next) => {
-  res.set({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Expose-Headers": "Content-Range",
-    "Content-Range": "1-2*",
-    "X-Total-Count": "30",
-  });
-  next();
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cors());
 
 configRoute(app);
 
